9d700868dcaadc46383c6036ceaef2edaf2f0a7f
Marija Vecovska <marija.vecovska@students.finki.ukim.mk>
2022-04-22 17:32:40 +0200

    first commit

---
+++ src/main/java/finki/ukim/mk/wpproekt/WpProektApplication.java
@@ -0,0 +1,13 @@
+package finki.ukim.mk.wpproekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WpProektApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WpProektApplication.class, args);
+    }
+
+}
---
+++ src/test/java/finki/ukim/mk/wpproekt/WpProektApplicationTests.java
@@ -0,0 +1,13 @@
+package finki.ukim.mk.wpproekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WpProektApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
